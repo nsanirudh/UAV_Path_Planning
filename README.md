9d275887f5a9cc61c010d42af0a7992ea9630c3e
nsanirudh <42909990+nsanirudh@users.noreply.github.com>
2019-03-31 15:50:19 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
-# UAV_Path_Planning
-This work is an Integration of two different softwares. STK and Matlab. STK is a simulation software where we simulate UAV flights and other scenarios. Matlab allows us to run the code for a shortest path algorithm through image processing.
+# UAV_flight_mission_planning
+
+This project involves the work I did at the *Defense Research and Development Organization (DRDO)*, New Delhi. The end product was a robust MATLAB-based tool to automatically generate UAV trajectories. The areas that my project touched: operations research, algorithms, image processing, interfacing. The tool is now being used by scientists at ISSA for less computationally expensive simulations. Software used: MATLAB and STK by AGI.
+ 
+The problem: 
+  1. Currently, in most places including DRDO, UAV trajectories are NOT mapped automatically before hand and multiple simulations are performed to find a path that avoids enemy radars (modelled as Domes). Thus, there is a need to find a more accurate and computationally inexpensive solution for the same.
+  2. The second issue is that there exists no functional tool for linking MATLAB/other programming suites and the simulations software - STK. There is a plugin offered by AGI, which doesn't seem to work in our case. 
+
+My approach (now a working solution):
+  1. Use the geo-map from the simulations software STK, and set up the scenario with starting point, ending point, and dome-shaped radars.
+  2. Import the image into MATLAB and use image pre-processing algorithms so that the A* algorithm for cost-optimization can be applied.
+  3. Use color codes for costs and color thresholding algorithms to differentiate between regions that a UAV can and cannot go through.
+  4. Find the required lat-long coordinates in MATLAB
+  5. *IMPORTANT* STK has a provision to take HTML commands for initializing path points for a UAV. We make use of this exploit and generate *HTML* commands as printed text, from the MATLAB application.
+  6. These commands are then fed to STK directly (simply copy-pasting) and we have thus solved the problem.
+
+Please feel free to write to me at sammit.bitspilani@gmail.com for any queries regarding this.
+Some pointers for using this repository:
+
+(@) Currently working approach:
+  1. Use STK to generate snap of the scenario to be evaluated. Use .tif for better results
+  2. Remember to fill the area targets with one colour and remove everything else from the scenario.
+  2. Run preprocess2.m in MATLAB and choose required colours. (Google impixel)
+  3. Run ip_approach1.m and feed it the path of the processed image.
+  
+(@) You'll get the plot in MATLAB, use photoshop to overlay it onto original image.
+
+(@) Currently working on conversion of waypoints according to scale and feeding them to STK
+
+EDIT:
+
+(@) Conversion algorithm added. This is essentially how it works:
+top_left = [42.64595 36.08668]; %Got this from STK cursor
+bottom_right = [19.18550 110.76360]; %Got this from STK cursor
+
+converted_points = [bottom_right(1)+(size(rad_map2,1)-OptimalPath(:,1)).*(top_left(1)-bottom_right(1))/size(rad_map2,1) top_left(2)+(bottom_right(2)-top_left(2)).*OptimalPath(:,2)/size(rad_map2,2)];
+
+Simple explanation: Use the top-left and bottom right lat-long coordinates to suitably scale and translate the coordinates. 
+NOTE: Images have their origin on the top left and coordinates traditionally start from bottom left (hence latitude needed to be subtracted to invert it, in a sense)
+
+(@) Algorithm added to generate STK connect commands
+Generating a text file called STK_instr.txt with looped waypoint generation commands
+Result from this text file is to be fed to the HTML API of STK
---
+++ ip_approach1.m
@@ -0,0 +1,146 @@
+%GET THE INPUT DATA FROM PREPROCESS2 REMEMBER. IMPORTANT
+clear
+
+rad_map = imread('C:\Users\Sammit\Downloads\DRDO_Confidential\processed.jpg');
+rad_map = rad_map(:,:,1);
+rad_map2 = int8(rad_map<127);
+%%% Generating a MAP
+%1 represents an object that the path cannot penetrate, zero is a free path
+
+MAP = rad_map2;
+
+%Start Positions
+StartX=476;
+StartY=106;
+
+%Generating goal nodes, which is represented by a matrix. Several goals can be speciefied, in which case the pathfinder will find the closest goal. 
+%a cell with the value 1 represent a goal cell
+GoalRegister=int8(zeros(size(rad_map2)));
+GoalRegister(298,552)=1;
+
+%Number of Neighboors one wants to investigate from each cell. A larger
+%number of nodes means that the path can be alligned in more directions. 
+%Connecting_Distance=1-> Path can  be alligned along 8 different direction.
+%Connecting_Distance=2-> Path can be alligned along 16 different direction.
+%Connecting_Distance=3-> Path can be alligned along 32 different direction.
+%Connecting_Distance=4-> Path can be alligned along 56 different direction.
+%ETC......
+
+Connecting_Distance=8; %Avoid to high values Connecting_Distances for reasonable runtimes. 
+
+% Running PathFinder
+OptimalPath=ASTARPATH(StartX,StartY,MAP,GoalRegister,Connecting_Distance)
+% End. 
+
+if size(OptimalPath,2)>1
+figure(10)
+imagesc((MAP))
+    colormap(flipud(gray));
+
+hold on
+plot(OptimalPath(1,2),OptimalPath(1,1),'o','color','k')
+plot(OptimalPath(end,2),OptimalPath(end,1),'o','color','b')
+plot(OptimalPath(:,2),OptimalPath(:,1),'r','LineWidth',2)
+legend('Goal','Start','Path')
+
+else 
+     pause(1);
+ h=msgbox('Sorry, No path exists to the Target!','warn');
+ uiwait(h,5);
+ end
+
+
+
+
+showNeighboors=0; %Set to 1 if you want to visualize how the possible directions of path. The code
+%below are purley for illustrating purposes. 
+if showNeighboors==1
+%
+
+
+
+%2
+NeigboorCheck=[0 1 0 1 0;1 1 1 1 1;0 1 0 1 0;1 1 1 1 1;0 1 0 1 0]; %Heading has 16 possible allignments
+[row col]=find(NeigboorCheck==1);
+Neighboors=[row col]-(2+1);
+figure(2)
+
+for p=1:size(Neighboors,1)
+  i=Neighboors(p,1);
+       j=Neighboors(p,2);
+      
+     plot([0 i],[0 j],'k')
+ hold on
+ axis equal
+ 
+grid on
+title('Connecting distance=2')
+end
+
+%3
+NeigboorCheck=[0 1 1 0 1 1 0;1 0 1 0 1 0 1;1 1 1 1 1 1 1;0 0 1 0 1 0 0;1 1 1 1 1 1 1;1 0 1 0 1 0 1;0 1 1 0 1 1 0]; %Heading has 32 possible allignments
+figure(3)
+[row col]=find(NeigboorCheck==1);
+Neighboors=[row col]-(3+1);
+
+for p=1:size(Neighboors,1)
+  i=Neighboors(p,1);
+       j=Neighboors(p,2);
+      
+     plot([0 i],[0 j],'k')
+ hold on
+ axis equal
+ grid on
+title('Connecting distance=3')
+
+end
+ 
+%4
+NeigboorCheck=[0 1 1 1 0 1 1 1 0;1 0 1 1 0 1 1 0 1;1 1 0 1 0 1 0 1 1;1 1 1 1 1 1 1 1 1;0 0 0 1 0 1 0 0 0;1 1 1 1 1 1 1 1 1;1 1 0 1 0 1 0 1 1 ;1 0 1 1 0 1 1 0 1 ;0 1 1 1 0 1 1 1 0];  %Heading has 56 possible allignments
+figure(4)
+[row col]=find(NeigboorCheck==1);
+Neighboors=[row col]-(4+1);
+
+for p=1:size(Neighboors,1)
+  i=Neighboors(p,1);
+       j=Neighboors(p,2);
+      
+     plot([0 i],[0 j],'k')
+ hold on
+ axis equal
+grid on
+title('Connecting distance=4')
+
+end
+%1
+NeigboorCheck=[1 1 1;1 0 1;1 1 1];
+figure(1)
+[row col]=find(NeigboorCheck==1);
+Neighboors=[row col]-(1+1);
+
+for p=1:size(Neighboors,1)
+  i=Neighboors(p,1);
+       j=Neighboors(p,2);
+      
+     plot([0 i],[0 j],'k')
+ hold on
+ axis equal
+grid on
+title('Connecting distance=1')
+
+end
+end
+
+%% Conversion algorithm
+top_left = [42.64595 36.08668]; %Got this from STK cursor
+bottom_right = [19.18550 110.76360]; %Got this from STK cursor
+
+converted_points = [bottom_right(1)+(size(rad_map2,1)-OptimalPath(:,1)).*(top_left(1)-bottom_right(1))/size(rad_map2,1) top_left(2)+(bottom_right(2)-top_left(2)).*OptimalPath(:,2)/size(rad_map2,2)];
+%%
+STK_instr = fopen('STK_instr.txt','a');
+
+formatSpec = 'AddWaypoint */Aircraft/MyAircraft_Con DetTimeAccFromVel %4.4f %4.4f 11000 257.22222\n';
+for i = 1:size(converted_points,1)
+    fprintf(STK_instr,formatSpec,converted_points(i,1),converted_points(i,2));
+end
+fclose(STK_instr);
